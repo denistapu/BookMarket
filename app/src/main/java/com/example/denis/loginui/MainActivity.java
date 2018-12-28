5dbb63a2d24d8c1b26b5d2c54555c8f8322d64b4
Denis <dynfyboy@gmail.com>
2018-12-29 00:25:33 +0100

    gg8

--- app/src/main/java/com/example/denis/loginui/MainActivity.java
+++ app/src/main/java/com/example/denis/loginui/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.denis.loginui;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Intent tStart;
+    public static final String TAG = "Books" ;
+    public static final int ACTIVITY_NUM = 0 ;
+
+    BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tStart = getIntent();
+        bottomNav = (BottomNavigationView) findViewById(R.id.navigationM);
+
+        BottomViewHelper.enableNavigation(MainActivity.this, bottomNav);
+
+        Menu menu = bottomNav.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
     }
+
 }
